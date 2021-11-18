09ca539b6958a4c7c9c4d01db72617f2dea20775
Elvis William <82127025+elvisshark@users.noreply.github.com>
2021-11-18 00:41:56 -0300

    Update Jogo.java

--- RPG/src/Jogo.java
+++ RPG/src/Jogo.java
@@ -2,7 +2,12 @@
 public class Jogo {
 
 	public static void main(String[] args) {
-		System.out.println("TESTE GITHUB DESKTOP");
+		int calc;
+		
+		calc = 4 * 4;
+		
+		System.out.println(calc);
+		
 	}
 
 }
